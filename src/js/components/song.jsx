009c2bbe9215fa47af3d8d7b507df8fa1493c791
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2025-02-05 18:13:24 +0000

    inicio

---
+++ src/js/components/song.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Song = () => {
+    return (
+        <div className="container bg-dark">
+            <div className="d-flex text-center">
+                <div>number </div>
+                <div> name -</div>
+            </div>
+        </div>
+    )
+}
+
+export default Song
---
+++ src/js/components/songList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Song from "./song";
+
+const SongList = () => {
+    return (
+        <div className="container">
+            Lista
+            <Song></Song>
+        </div>
+
+    )
+}
+
+export default SongList
