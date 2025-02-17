d47a94384661780709a4381e5525689d28bd6c41
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2025-02-17 23:11:28 +0000

    search bar falta integrar el filter

--- src/js/components/searchBar.jsx
+++ src/js/components/searchBar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import SongList from "./songList";
 
 const SearchBar = () => {
+
+    // const [input,setInput] = useState([])
+
+    // const handleInputChange = (e) =>{
+    //     setInput(e.target.value)
+    // };
+
+    // const songFiltered = songs.filter(s => s.includes(input))
+
     return (
         <div className="input-group mb-3">
             <span className="input-group-text" id="inputGroup-sizing-default">Search</span>
-            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" 
+            // onChange={handleInputChange}
+            />
         </div>   
     )
 }
